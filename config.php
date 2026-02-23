ef6ebe8569d2b9b1a5c38b3f58beb550e59d3602
Kavana Boodappa Bagalakoti <163529351+KavanaSdmcet@users.noreply.github.com>
2026-02-23 12:31:11 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,12 +1,15 @@
 <?php
-$host = "localhost";
-$user = "root";
-$password = "";
-$db = "skillsonics_verify";
 
-$conn = new mysqli($host, $user, $password, $db);
+$host = "hopper.proxy.rlwy.net";
+$user = "root";
+$password = "YXKHkPmYbeCMOHYDKmFTxTQIHaLnnRgt";
+$db = "railway";
+$port = 54560;
+
+$conn = new mysqli($host, $user, $password, $db, $port);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
 ?>
